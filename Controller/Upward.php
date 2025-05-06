c03d9311f1e7b8ac789210622322d7e5d41de074
Bharathidasan Elangovan <del22123@adobe.com>
2025-05-06 15:51:40 +0530

    Fixed multi website issue (#26)

--- Controller/Upward.php
+++ Controller/Upward.php
@@ -13,6 +13,10 @@ use Magento\Framework\App\ResponseInterface;
 use Magento\Framework\HTTP\PhpEnvironment\Response;
 use Laminas\Http\Response\Stream;
 use Magento\UpwardConnector\Model\Prerender;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Framework\UrlInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\ScopeInterface;
 
 class Upward implements FrontControllerInterface
 {
@@ -32,19 +36,43 @@ class Upward implements FrontControllerInterface
     private $prerender;
 
     /**
+     * @var StoreManagerInterface
+     */
+    private $storeManager;
+
+    /**
+     * @var \Magento\Framework\UrlInterface
+     */
+    private $url;
+
+    /**
+     * @var \Magento\Framework\App\Config\ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
      * Upward constructor.
      * @param Response $response
      * @param UpwardControllerFactory $upwardFactory
      * @param Prerender $prerender
+     * @param StoreManagerInterface $storeManager
+     * @param UrlInterface $url
+     * @param ScopeConfigInterface $scopeConfig
      */
     public function __construct(
         Response $response,
         UpwardControllerFactory $upwardFactory,
-        Prerender $prerender
+        Prerender $prerender,
+        StoreManagerInterface $storeManager,
+        UrlInterface $url,
+        ScopeConfigInterface $scopeConfig
     ) {
         $this->response = $response;
         $this->upwardFactory = $upwardFactory;
         $this->prerender = $prerender;
+        $this->storeManager = $storeManager;
+        $this->url = $url;
+        $this->scopeConfig = $scopeConfig;
     }
 
     /**
@@ -55,6 +83,24 @@ class Upward implements FrontControllerInterface
      */
     public function dispatch(RequestInterface $request)
     {
+        $baseUrl = $this->storeManager->getStore()->getBaseUrl(
+            UrlInterface::URL_TYPE_WEB,
+            $this->storeManager->getStore()->isCurrentlySecure()
+        );
+        // phpcs:disable Magento2.Functions.DiscouragedFunction
+        $uri = parse_url($baseUrl);
+        if ($request->getUri()->getHost() !== $uri['host']) {
+            $redirectUrl = $this->url->getRedirectUrl(
+                $this->url->getDirectUrl(ltrim($request->getPathInfo(), '/'), ['_nosid' => true])
+            );
+            $redirectCode = (int)$this->scopeConfig->getValue(
+                'web/url/redirect_to_base',
+                ScopeInterface::SCOPE_STORE
+            ) !== 301 ? 302 : 301;
+            $this->response->setRedirect($redirectUrl, $redirectCode);
+            return $this->response;
+        }
+
         $prerenderedResponse = null;
         if ($this->prerender->shouldShowPrerenderedPage($request)) {
             /** @var \Laminas\Http\Response $prerenderedResponse */
