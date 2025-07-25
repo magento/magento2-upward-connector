c259e6ee28058713c47dbfaa59539368563076c4
Bharathidasan Elangovan <del22123@adobe.com>
2025-07-25 21:25:56 +0530

    Fixed PHP 8.4 compatibility issue in Mustache module (#28)

--- registration.php
+++ registration.php
@@ -6,4 +6,12 @@
 
 use \Magento\Framework\Component\ComponentRegistrar;
 
+// Class aliases to override Mustache classes for PHP 8.4 compatibility
+if (!class_exists('Mustache_Engine', false)) {
+    class_alias('Magento\UpwardConnector\Mustache\Engine', 'Mustache_Engine');
+}
+if (!class_exists('Mustache_Parser', false)) {
+    class_alias('Magento\UpwardConnector\Mustache\Parser', 'Mustache_Parser');
+}
+
 ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Magento_UpwardConnector', __DIR__);
